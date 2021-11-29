f20ec4efb83414fbbd14c110970c36487cddf66a
mdodaro <mike.dodaro@myzenkey.com>
2021-11-29 13:27:35 -0800

    Config and template files

---
+++ src/main/java/com/example/zenketest/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.zenketest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/terms").setViewName("terms");
+        registry.addViewController("/privacy").setViewName("privacy");
+		registry.addViewController("/login").setViewName("login");
+	}
+}
