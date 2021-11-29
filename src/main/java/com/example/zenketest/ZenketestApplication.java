db84bafe71247320983b866b4276b9e8c3a32083
Mike Dodaro <91149328+MikeDodaroZen@users.noreply.github.com>
2021-11-29 13:18:25 -0800

    Add files for Maven build

---
+++ src/main/java/com/example/zenketest/ZenketestApplication.java
@@ -0,0 +1,13 @@
+package com.example.zenketest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenketestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenketestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zenketest/ZenketestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zenketest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenketestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
